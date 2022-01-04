fe670ee5abcaa7e7c747bfd3442f06bb55ec3987
Jocélia da Hora <joceliahora@gmail.com>
2022-01-04 00:12:19 -0300

    fases

---
+++ game_resilia/oona/oona.js
@@ -0,0 +1,4 @@
+function oona(){
+    alert("Sabendo que Oona precisa escolher entre 3 caminhos diferentes para salvar o Rei, digite o numero que corresponde ao caminho para salva-lo");
+    let fase = prompt("1. Floresta \n2. Mar \n3.Beco dos Elfos")
+}
---
+++ game_resilia/script.js
@@ -0,0 +1,13 @@
+alert("Olá, sudito escolha uma das princesas clicando em seu nome abaixo");
+
+function oona(){
+    window.location.href="oona.html"
+}
+
+function dagmar(){
+    window.location.href="dagmar.html"
+}
+
+function tiabine(){
+    window.location.href="tiabine.html"
+}
