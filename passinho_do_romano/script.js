d58e0cd83a13b1d163b3ffbc511ceba2f78d87d9
Jocélia da Hora <joceliahora@gmail.com>
2022-01-23 18:10:05 -0300

    passinho do romano

--- passinho_do_romano/script.js
+++ passinho_do_romano/script.js
@@ -1,15 +1,30 @@
 function confirma (){
-    var textAreaContet = document.getElementById("id_textarea").value;
+    var selectContent = document.getElementById("id_select").value;
+    var textAreaContent = document.getElementById("id_textarea").value;
     var deslocamento = document.getElementById ("deslocamento_id").value;
-    var mensagem = document.getElementById("resultado_id");
+    var resultadoArea = document.getElementById("resultado_id");
     var radio = document.getElementById("codificador").checked; 
-    console.log(radio);
-    if (radio == true){
-        mensagem.innerHTML = codificadorCezar(textAreaContet, parseInt(deslocamento));
-    }else{
-        mensagem.innerHTML = decodificadorCezar(textAreaContet, parseInt(deslocamento));
+    
+    
+    if (selectContent == "base64"){
+        if(radio == true){
+            resultadoArea.innerHTML = codificadorBase64(textAreaContent);
+        } else {
+            var teste = decodificadorBase64(textAreaContent);
+            console.log(teste)
+            resultadoArea.innerHTML = teste;
+        }
+    }else if (selectContent == "cesar"){
+        if (radio ==true){
+            resultadoArea.innerHTML = codificadorCezar(textAreaContent.toLowerCase(), parseInt(deslocamento));
+        } else {
+            var teste = decodificadorCezar(textAreaContent.toLowerCase(), parseInt(deslocamento));
+            resultadoArea.innerHTML = "";
+            resultadoArea.innerHTML = teste;
+        }
     }
 }
+
 
 function codificadorCezar(palavra, deslocamento){// função do ponto de vista de quem desenvolve
     var letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z']
@@ -38,7 +53,17 @@ function decodificadorCezar(palavra, deslocamento){
             resultado = resultado + letras[i-deslocamento];
             i = 26;
         }
-    }
+    } console.log(resultado);
     return resultado;
 
+}
+
+function codificadorBase64(palavra){
+    var resultado = btoa(palavra)
+    return resultado;
+}
+
+function decodificadorBase64(palavra){
+    var resultado = atob(palavra)
+    return resultado;
 }
